be50149cb3797ce3119b299858b9e431d47ffb7a
univrsal <univrsal@protonmail.ch>
2017-06-17 01:27:02 +0200

    Reformatted contact

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 function onReady(callback) {
-    var intervalID = window.setInterval(checkReady, 250);
+    var intervalID = window.setInterval(checkReady, 1000);
     function checkReady() {
         if (document.getElementsByTagName('body')[0] !== undefined) {
             window.clearInterval(intervalID);
